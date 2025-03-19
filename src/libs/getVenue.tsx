21e88a6275f2aae8f0b8c07b565ae0ab07a653be
guniism <gun16110@hotmail.com>
2025-03-19 20:29:43 +0700

    Change url

--- src/libs/getVenue.tsx
+++ src/libs/getVenue.tsx
@@ -1,5 +1,5 @@
 export default async function getVenue(id:string){
-    const response = await fetch(`https://a08-venue-explorer-backend-3.vercel.app/api/v1/venues/${id}`);
+    const response = await fetch(`https://a08-venue-explorer-backend-2.vercel.app/api/v1/venues/${id}`);
     if(!response.ok){
         throw new Error("Failed to fetch venue");
     }
